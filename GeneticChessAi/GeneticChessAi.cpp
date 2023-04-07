4fea648ee51ec5a96249a345190de4c5bf8f1295
Jitse Verstraete <jitse.verstraete@gmail.com>
2023-04-07 21:56:35 +0200

    setup libraries

--- GeneticChessAi/GeneticChessAi.cpp
+++ GeneticChessAi/GeneticChessAi.cpp
@@ -2,9 +2,28 @@
 //
 
 #include <iostream>
+#include <thc.h>
 
-int main()
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow("GAME",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        1000, 1000, 0);
+
+
+
+    return 0;
+	
+
+
 }
 
