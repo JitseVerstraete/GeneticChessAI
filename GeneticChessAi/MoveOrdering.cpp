a62792268e792e0df01ee7ecd88aa8de306ec2ac
jitseV <jitse.verstraete@gmail.com>
2023-04-13 15:37:17 +0200

    move ordering logic

--- GeneticChessAi/MoveOrdering.cpp
+++ GeneticChessAi/MoveOrdering.cpp
@@ -1,10 +1,11 @@
 #include "MoveOrdering.h"
 
 #include <algorithm>
+#include "EvalFunctions.h"
 
 void OrderMoves(std::vector<thc::Move>& moves, thc::ChessRules& pos)
 {
-	auto greater = [&pos](const thc::Move& lhs, const thc::Move& rhs) -> bool
+	auto greater = [&pos](thc::Move& lhs, thc::Move& rhs) -> bool
 	{
 		return MoveValuePrediction(lhs, pos) > MoveValuePrediction(rhs, pos);
 	};
@@ -12,37 +13,43 @@ void OrderMoves(std::vector<thc::Move>& moves, thc::ChessRules& pos)
 	std::sort(moves.begin(), moves.end(), greater);
 }
 
-float MoveValuePrediction(const thc::Move& move, thc::ChessRules& pos)
+float MoveValuePrediction(thc::Move& move, thc::ChessRules& pos)
 {
-
-	const float checkValue{};
-	const float captureValue{};
-	const float AttackValue{};
+	const float checkValue{ 50.f };
+	const float captureValue{ 10.f };
+	//mconst float AttackValue{0.2f};
 
 
 
 	float value{};
 	bool whiteToMove{ pos.WhiteToPlay() };
 
+
 	//checks 
 	char enemyKing = whiteToMove ? 'k' : 'K';
-
+	pos.PushMove(move);
 	for (int i{}; i < 64; ++i)
 	{
 		if (pos.squares[i] != enemyKing) continue;
 
-		if (pos.AttackedSquare(thc::Square(pos.squares[i]), whiteToMove))
+		//check if this move puts the king in check
+
+		if (pos.AttackedPiece((thc::Square)i))
 		{
 			value += checkValue;
 		}
 
 	}
+	pos.PopMove(move);
+	
 
-	//captures
-
-	//attacks
-
-
+	//give a bonus prediction to captures based on piece type
+	if (isalpha(move.capture))
+	{
+		float capturedPieceValue = MaterialEval::pieceValues.find(tolower(move.capture))->second;
+		float capturingPieceValue = MaterialEval::pieceValues.find(tolower(pos.squares[move.src]))->second;
+		value += captureValue * capturedPieceValue - capturingPieceValue;
+	}
 
 	return value;
 }
--- GeneticChessAi/MoveOrdering.h
+++ GeneticChessAi/MoveOrdering.h
@@ -4,7 +4,7 @@
 #include <thc.h>
 
 
-void OrderMoves(std::vector<thc::Move>& moves, const thc::ChessRules& pos);
+void OrderMoves(std::vector<thc::Move>& moves, thc::ChessRules& pos);
 
-float MoveValuePrediction(const thc::Move& move, const thc::ChessRules& pos);
+float MoveValuePrediction(thc::Move& move, thc::ChessRules& pos);
 
