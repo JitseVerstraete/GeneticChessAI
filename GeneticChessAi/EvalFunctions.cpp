6407576050df6cbf683b858e31d81f3647e0e559
jitseV <jitse.verstraete@gmail.com>
2023-04-13 15:36:05 +0200

    mad piece value map static

--- GeneticChessAi/EvalFunctions.cpp
+++ GeneticChessAi/EvalFunctions.cpp
@@ -1,5 +1,7 @@
 #include "EvalFunctions.h"
 
+const std::map<char, float> MaterialEval::pieceValues = { {'p', 1}, { 'b', 3 }, { 'n', 3 }, { 'r', 5 }, { 'q', 9 }, { 'k', 1'000'000 } };
+
 float MaterialEval::operator()(const thc::ChessRules& position)
 {
 	float total{};
@@ -11,7 +13,7 @@ float MaterialEval::operator()(const thc::ChessRules& position)
 		if (piece == ' ') continue;
 
 		bool white = isupper(piece);
-		float value = m_pieceValues.find(tolower(piece))->second;
+		float value = pieceValues.find(tolower(piece))->second;
 
 		total += white ? value : -value;
 	}
